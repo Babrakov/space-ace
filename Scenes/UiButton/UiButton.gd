16c4be9048d315fe5a441e4059aea649be8577e8
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-06-03 09:04:07 +0200

    UI Buttons

---
+++ Scenes/UiButton/UiButton.gd
@@ -0,0 +1,10 @@
+extends TextureButton
+
+class_name UiButton
+
+@export var button_text: String = "Set Me"
+
+@onready var label: Label = $Label
+
+func _ready() -> void:
+	label.text = button_text
