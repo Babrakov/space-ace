2d3f72e6044330594256e8712ed1bd24d4355d06
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-06-05 08:58:15 +0200

    Explosion

--- Scenes/ObjectMaker/ObjectMaker.gd
+++ Scenes/ObjectMaker/ObjectMaker.gd
@@ -2,11 +2,16 @@ extends Node2D
 
 
 const ADD_OBJECT: String = "add_object"
-
+const EXPLOSION = preload("res://Scenes/Explosion/Explosion.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	SignalHub.on_create_explosion.connect(on_create_explosion)
+
+func on_create_explosion(pos: Vector2, anim_name: String ) -> void:
+	var scene: Explosion = EXPLOSION.instantiate()
+	scene.setup(anim_name)
+	call_deferred(ADD_OBJECT, scene, pos)
 
 
 func add_object(obj: Node, pos: Vector2) -> void:
