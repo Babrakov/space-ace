b0888ef509fb2491217f19036fa745477c4efc1f
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-07-25 08:50:21 +0200

    Player Health Update

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("test"):
-		#SignalHub.emit_on_create_bullet(
-			#Vector2(315,300),
-			#Vector2.DOWN,
-			#100.0,
-			#BulletBase.BulletType.Bomb
-		#)
+		SignalHub.emit_on_create_bullet(
+			Vector2(315,250),
+			Vector2.DOWN,
+			100.0,
+			BulletBase.BulletType.Bomb
+		)
 		SignalHub.emit_on_create_powerup(
 			Vector2(315,300), 
 		   PowerUp.PowerUpType.Shield
