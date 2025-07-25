ac900f87b2982d3a52a01ca2e443e4d82b480ec9
Sergei Babrakov <s.a.babrakov@gmail.com>
2025-07-25 08:38:02 +0200

    Health Bar

--- Scenes/Shield/Shield.gd
+++ Scenes/Shield/Shield.gd
@@ -12,7 +12,7 @@ var _health: int = start_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	enable_shield()
+	disable_shield()
 
 func enable_shield() -> void:
 	animation_player.play("RESET")
@@ -22,12 +22,10 @@ func enable_shield() -> void:
 	SpaceUtils.toggle_area2d(self, true)
 	sound.play()
 
-
 func disable_shield() -> void:
 	timer.stop()
 	hide()
 	SpaceUtils.toggle_area2d(self, false)
-
 
 func hit() -> void:
 	animation_player.play("hit")
@@ -35,10 +33,8 @@ func hit() -> void:
 	if _health <= 0:
 		disable_shield()
 
-
 func _on_timer_timeout() -> void:
 	disable_shield()
-
 
 func _on_area_entered(area: Area2D) -> void:
 	hit()
